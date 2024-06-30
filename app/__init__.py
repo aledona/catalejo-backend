e067e0015d2959a30cd593d9c068fa7b6425425f
aledona <donascimento.alejandro@gmail.com>
2024-06-30 20:49:09 -0300

    version final

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ def create_app():
 
     app.route('/api/countries/', methods=['GET'])(get_all_countries)
     app.route('/api/users/', methods=['POST'])(create_user)
-    app.route('/api/users/login/', methods=['POST'])(login_user)
+    app.route('/api/users/login', methods=['POST'])(login_user)
     app.route('/api/users/<int:id>', methods=['PUT'])(update_lastlogin)
     app.route('/api/users/<int:id>', methods=['DELETE'])(delete_user)
     
